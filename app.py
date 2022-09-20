d92d9a8b164714dd8a8e17a71239e526214cae1d
kumar-tiger <kumar.rajwani@tigeranalytics.com>
2022-09-20 15:27:34 +0530

    first commit

--- app.py
+++ app.py
@@ -1,29 +1,49 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+# from datetime import datetime
+# import os
+from flask import (
+    Flask,
+    jsonify,
+    # render_template,
+    # request,
+    # redirect,
+    # url_for,
+    # send_from_directory,
+)
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    return jsonify({"message": "Hello world!"})
 
 
-if __name__ == '__main__':
-   app.run()
+#    print('Request for index page received')
+#    return render_template('index.html')
+
+
+# @app.route("/favicon.ico")
+# def favicon():
+#     return send_from_directory(
+#         os.path.join(app.root_path, "static"),
+#         "favicon.ico",
+#         mimetype="image/vnd.microsoft.icon",
+#     )
+
+
+# @app.route("/hello", methods=["POST"])
+# def hello():
+#     name = request.form.get("name")
+
+#     if name:
+#         print("Request for hello page received with name=%s" % name)
+#         return render_template("hello.html", name=name)
+#     else:
+#         print(
+#             "Request for hello page received with no name or blank name -- redirecting"
+#         )
+#         return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
